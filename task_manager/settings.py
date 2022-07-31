012507dbe589c8ee0906ab8e6d491c561fccfe5a
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-07-31 18:43:05 +0300

    Add python-dotenv and gitignore

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jqn4xcci_6np#-3scxibe9m0lk+-y@5+&(kc)#*(dy91lpj4&c'
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
