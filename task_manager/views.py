3010666b6829648a6c2e6419f6ea1d430e0464fa
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-08-25 19:01:42 +0300

    Fixed templates

--- task_manager/views.py
+++ task_manager/views.py
@@ -207,7 +207,7 @@ class TaskCreate(SuccessMessageMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['btn'] = _("Создать")
+        context['btn'] = _("Создать задачу")
         return context
 
     def dispatch(self, request, *args, **kwargs):
